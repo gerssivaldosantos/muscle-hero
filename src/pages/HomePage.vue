2bdb4721c88e36b8edc53c32cf95f06e3b8f6f3e
gerssivaldo <canalandows@gmail.com>
2023-03-05 19:46:44 -0300

    feat: add header with progress linear bar

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,18 +1,25 @@
 <template>
   <q-page>
-    <q-card>
-      <q-card-section>
-        <h4>Home</h4>
-      </q-card-section>
-    </q-card>
+    <q-header class="q-ma-md bg-transparent">
+      <q-linear-progress  size="25px" :value="progress" color="primary">
+        <div class="absolute-full flex flex-center">
+         <p class="text-subtitle1 text-bold text-white">{{progress * 100}}%</p>
+        </div>
+      </q-linear-progress>
+      <div class="text-caption">Progresso semanal</div>
+    </q-header>
   </q-page>
 </template>
 
 <script setup lang="ts">
 
+import { ref } from 'vue'
+
+const progress = ref(0.5)
+
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
 .q-page{
   display: flex;
   justify-content: center;
@@ -21,6 +28,11 @@
 
 .q-card {
   width: 80%;
+}
+
+.logo-section {
+  justify-content: center;
+  display: flex;
 }
 
 </style>
