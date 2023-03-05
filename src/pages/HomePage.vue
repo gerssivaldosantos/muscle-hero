012254e4514227c43dd31e0a003761259eb90082
gerssivaldo <canalandows@gmail.com>
2023-03-05 18:28:53 -0300

    init

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,26 @@
+<template>
+  <q-page>
+    <q-card>
+      <q-card-section>
+        <h4>Home</h4>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+.q-page{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.q-card {
+  width: 80%;
+}
+
+</style>
