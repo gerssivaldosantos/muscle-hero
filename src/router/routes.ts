047f99d6e3f3809ae8dc1fbd9b6ea6cb57b84cab
gerssivaldo <canalandows@gmail.com>
2023-03-12 15:32:24 -0300

    feat: add "add-training" route

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -6,9 +6,11 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/HomePage.vue') }]
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
+  {
+    path: '/add-training',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [{ path: '', component: () => import('pages/NewTrainingForm.vue') }]
+  },
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue')
