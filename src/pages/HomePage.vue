96b17b156b967885a2da1d5b3b9b958debd519d7
gerssivaldo <canalandows@gmail.com>
2023-03-14 18:33:36 -0300

    style: fix lint

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,39 +1,39 @@
 <template>
-  <q-page>
-    <q-header class="header shadow-5">
-      <div class="full-width row q-py-md q-pa-md">
-        <div class="col">
-          <q-avatar>
-            <img src="https://avatars.githubusercontent.com/u/61440136?v=4" alt="user-photo">
-          </q-avatar>
-          <div class="text-subtitle1">Gerssivaldo Santos</div>
+    <q-page>
+      <q-header class="header shadow-5">
+        <div class="full-width row q-py-md q-pa-md">
+          <div class="col">
+            <q-avatar>
+              <img src="https://avatars.githubusercontent.com/u/61440136?v=4" alt="user-photo">
+            </q-avatar>
+            <div class="text-subtitle1">Gerssivaldo Santos</div>
+          </div>
+          <div class="col">
+            <q-linear-progress stripe class="progress-bar"  size="25px" :value="progress" color="primary">
+              <div class="absolute-full flex flex-center">
+                <div class="text-subtitle1 text-bold text-white">{{progress * 100}}%</div>
+              </div>
+            </q-linear-progress>
+            <div class="q-pa-sm text-caption flex-center ">Weekly Progress</div>
+          </div>
         </div>
-        <div class="col">
-          <q-linear-progress stripe class="progress-bar"  size="25px" :value="progress" color="primary">
-            <div class="absolute-full flex flex-center">
-              <div class="text-subtitle1 text-bold text-white">{{progress * 100}}%</div>
-            </div>
-          </q-linear-progress>
-          <div class="q-pa-sm text-caption flex-center ">Weekly Progress</div>
+        <div class="row">
+          <div class="title text-h4 q-pl-lg q-pb-lg text-bold">
+            Home
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="title text-h4 q-pl-lg q-pb-lg text-bold">
-          Home
-        </div>
-      </div>
 
-    </q-header>
+      </q-header>
 
-    <div class="cards-wrapper">
-      <RouteCard
-        class="card"
-        v-for="routeCard in routeCards"
-        :key="routeCard.name"
-        :name="routeCard.name"
-        :icon="routeCard.icon"/>
-    </div>
-  </q-page>
+      <div class="cards-wrapper">
+        <RouteCard
+          class="card"
+          v-for="routeCard in routeCards"
+          :key="routeCard.name"
+          :name="routeCard.name"
+          :icon="routeCard.icon"/>
+      </div>
+    </q-page>
 </template>
 
 <script setup lang="ts">
