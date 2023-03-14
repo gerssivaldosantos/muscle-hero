bcf1d18c43d3f4f5d19f544e05654ed2c1cc8600
gerssivaldo <canalandows@gmail.com>
2023-03-14 18:34:26 -0300

    feat: add initial implementation for workout repository

---
+++ src/core/workout/workout.repository.ts
@@ -0,0 +1,24 @@
+import { deleteActionResponse, findParams, RepositoryInterface } from 'src/core/base/repository.interface'
+import Workout from 'src/core/workout/models/workout.model'
+
+export class WorkoutRepository implements RepositoryInterface<Workout> {
+  delete (id: string): Promise<deleteActionResponse> {
+    return Promise.resolve({ success: true })
+  }
+
+  findMany (params: findParams): Promise<Workout[]> {
+    return Promise.resolve([])
+  }
+
+  findOne (id: string): Promise<Workout | undefined> {
+    return Promise.resolve(undefined)
+  }
+
+  insert (item: Workout): Promise<Workout | undefined> {
+    return Promise.resolve(undefined)
+  }
+
+  update (id: string, item: Workout): Promise<boolean> {
+    return Promise.resolve(false)
+  }
+}
