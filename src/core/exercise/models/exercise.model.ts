1b6b914e1ee8355e2607b08bea6378169c05d742
gerssivaldo <canalandows@gmail.com>
2023-03-14 22:53:22 -0300

    feat: add id

--- src/core/exercise/models/exercise.model.ts
+++ src/core/exercise/models/exercise.model.ts
@@ -1,6 +1,10 @@
-import { IsInt, IsPositive, Length, validate } from 'class-validator'
+import { IsInt, IsPositive, IsString, Length, validate } from 'class-validator'
+import { v4 as uuid } from 'uuid'
 
 export default class Exercise {
+  @IsString()
+    id?: string
+
   @Length(1, 50)
     name: string
 
@@ -16,15 +20,16 @@ export default class Exercise {
   @IsPositive()
     restTime: number
 
-  constructor (name: string, sets: number, reps: number, restTime: number) {
+  constructor (id: string | undefined, name: string, sets: number, reps: number, restTime: number) {
+    this.id = id || uuid()
     this.name = name
     this.sets = sets
     this.reps = reps
     this.restTime = restTime
   }
 
-  public static async create (data: Record<string, any>): Promise<Exercise> {
-    const exercise = new Exercise(data.name, data.sets, data.reps, data.restTime)
+  public static async create (data: Record<string, never>): Promise<Exercise> {
+    const exercise = new Exercise(data.id, data.name, data.sets, data.reps, data.restTime)
     const errors = await validate(exercise)
     if (errors.length > 0) {
       throw new Error(errors.toString())
--- src/core/workout/models/workout.model.ts
+++ src/core/workout/models/workout.model.ts
@@ -1,7 +1,11 @@
-import { Length, validate, IsArray, ArrayMinSize, IsInstance } from 'class-validator'
+import { Length, validate, IsArray, ArrayMinSize, IsInstance, IsString } from 'class-validator'
 import Exercise from 'src/core/exercise/models/exercise.model'
+import { v4 as uuid } from 'uuid'
 
 export default class Workout {
+  @IsString()
+    id?: string
+
   @Length(1, 50)
     name: string
 
@@ -10,17 +14,18 @@ export default class Workout {
   @IsInstance(Exercise, { each: true })
     exercises: Exercise[]
 
-  constructor (name: string, exercises: Exercise[]) {
+  constructor (id: string | undefined, name: string, exercises: Exercise[]) {
+    this.id = id || uuid()
     this.name = name
     this.exercises = exercises
   }
 
   public static async create (data: Record<string, any>): Promise<Workout> {
-    const exercises = data.exercises.map((exercise: Record<string, any>) =>
+    const exercises = data.exercises.map((exercise: Record<string, never>) =>
       Exercise.create(exercise)
     )
 
-    const workout = new Workout(data.name, exercises)
+    const workout = new Workout(data.id, data.name, exercises)
     const errors = await validate(workout)
     if (errors.length > 0) {
       throw new Error(errors.toString())
