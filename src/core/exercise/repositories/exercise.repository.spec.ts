5ec7840f1654653a47bf4d1bbc447f4201125e77
gerssivaldo <canalandows@gmail.com>
2023-03-15 23:04:01 -0300

    test: add findOne tests

--- src/core/exercise/repositories/exercise.repository.spec.ts
+++ src/core/exercise/repositories/exercise.repository.spec.ts
@@ -2,6 +2,7 @@ import { describe, expect, it, beforeAll } from 'vitest'
 import { FindParams, RepositoryInterface } from 'src/core/base/repository.interface'
 import Exercise from 'src/core/exercise/models/exercise.model'
 import ExerciseMockRepository from 'src/core/exercise/repositories/exercise-mock.repository'
+import { ValidationError } from 'class-validator'
 
 let repository: RepositoryInterface<Exercise>
 
@@ -36,6 +37,30 @@ describe('Exercise Repository Test', () => {
         const created = await Exercise.create(item)
         expect(created).toBeInstanceOf(Exercise)
       }
+    })
+  })
+
+  describe('FindOne Tests', () => {
+    it('Should return success find', async () => {
+      const result = repository.findOne('271ed917-58aa-4611-bb3f-52c344668e87')
+      expect(result).toBeTruthy()
+      const exercise:Exercise | Array<ValidationError> = await Exercise.create(result)
+      expect(exercise).toBeInstanceOf(Exercise)
+    })
+
+    it('Should return a error with data finded', async () => {
+      let result: Exercise | undefined = await repository.findOne('271ed917-58aa-4611-bb3f-52c344668e87')
+      expect(result).toBeTruthy()
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      result = { ...result, equipment: 'error' }
+      const exercise:Exercise | Array<ValidationError> = await Exercise.create(result)
+      expect(exercise).toBeInstanceOf(Array<ValidationError>)
+    })
+
+    it('Should not return any data', async () => {
+      const result: Exercise | undefined = await repository.findOne('error')
+      expect(result).toBeFalsy()
     })
   })
 })
