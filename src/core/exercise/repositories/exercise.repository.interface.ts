999a1839f6b2bf58e75fb680532a8aa8af4fe795
gerssivaldo <canalandows@gmail.com>
2023-03-15 22:45:34 -0300

    feat: add exercise repository interface

---
+++ src/core/exercise/repositories/exercise.repository.interface.ts
@@ -0,0 +1,6 @@
+import { RepositoryInterface } from 'src/core/base/repository.interface'
+import Exercise from 'src/core/exercise/models/exercise.model'
+
+type ExerciseRepositoryInterface = RepositoryInterface<Exercise>
+
+export default ExerciseRepositoryInterface
