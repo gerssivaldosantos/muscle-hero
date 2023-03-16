78ce6893cc8d2124a74a9af5acbd88a4ab90e68a
gerssivaldo <canalandows@gmail.com>
2023-03-15 22:45:55 -0300

    feat: add exercise repository (only findMany method implemented)

---
+++ src/core/exercise/repositories/exercise-mock.repository.ts
@@ -0,0 +1,27 @@
+import { deleteActionResponse, FindParams } from 'src/core/base/repository.interface'
+import Exercise from 'src/core/exercise/models/exercise.model'
+import ExerciseRepositoryInterface from 'src/core/exercise/repositories/exercise.repository.interface'
+import { exercisesMock } from 'src/mocks/exercises'
+
+export default class ExerciseMockRepository implements ExerciseRepositoryInterface {
+  findMany (params: FindParams): Promise<Exercise[]> {
+    const response:Exercise[] = exercisesMock.slice(params.offset, params.limit + params.offset)
+    return Promise.resolve(response)
+  }
+
+  findOne (id: string): Promise<Exercise | undefined> {
+    return Promise.resolve(undefined)
+  }
+
+  insert (item: Exercise): Promise<Exercise | undefined> {
+    return Promise.resolve(undefined)
+  }
+
+  update (id: string, item: Exercise): Promise<boolean> {
+    return Promise.resolve(false)
+  }
+
+  delete (id: string): Promise<deleteActionResponse> {
+    return Promise.resolve({ success: false })
+  }
+}
