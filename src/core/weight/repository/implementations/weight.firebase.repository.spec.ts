9a154b1e3f823ea4c58573a90278b949ddbdf9cf
gerssivaldo <canalandows@gmail.com>
2023-07-01 03:34:54 -0300

    feat: add basic crud repository spec

--- src/core/weight/repository/implementations/weight.firebase.repository.spec.ts
+++ src/core/weight/repository/implementations/weight.firebase.repository.spec.ts
@@ -4,9 +4,11 @@ import { faker } from '@faker-js/faker'
 import { WeightModel } from 'src/core/weight/model/weight.model'
 import { FirebaseRepository } from 'src/core/base/repository/implementations/firebase.repository'
 import { FirebaseApp, initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { Firestore, getFirestore } from 'firebase/firestore'
 import { WeightFirebaseRepository } from 'src/core/weight/repository/implementations/weight.firebase.repository'
 import dotenv from 'dotenv'
+import { FindParams } from 'src/core/base/repository/repository.interface'
+
 dotenv.config()
 
 const clientCredentials = {
@@ -28,14 +30,13 @@ const createWeightData = (): createWeight => {
 describe('Repository Tests', () => {
   let firebaseApp: FirebaseApp
   let repository: WeightFirebaseRepository
+  let fireStore: Firestore
   let actualModel: WeightModel
-  it('Test default instance is sucessfully created', async () => {
-    expect(repository).toBeInstanceOf(FirebaseRepository)
-  })
 
   beforeAll(() => {
     firebaseApp = initializeApp(clientCredentials)
-    repository = new WeightFirebaseRepository(getFirestore(firebaseApp))
+    fireStore = getFirestore(firebaseApp)
+    repository = new WeightFirebaseRepository(fireStore)
   })
 
   beforeEach(async () => {
@@ -43,11 +44,15 @@ describe('Repository Tests', () => {
     await repository.insert(actualModel)
   })
 
+  it('Test default instance is sucessfully created', async () => {
+    expect(repository).toBeInstanceOf(FirebaseRepository)
+  })
+
   describe('Insert', () => {
     it('Should insert successfully weight', async () => {
       const modelToInsert = new WeightModel(createWeightData())
       const insertResult = await repository.insert(modelToInsert)
-      expect(insertResult).toBeTruthy()
+      expect(insertResult.success).toBeTruthy()
       const findResult = await repository.findOne(modelToInsert.id)
       expect(findResult).toBeTruthy()
     })
@@ -57,6 +62,37 @@ describe('Repository Tests', () => {
     it('Should find successfully weight by id', async () => {
       const result = await repository.findOne(actualModel.id)
       expect(result).toBeTruthy()
+    })
+  })
+
+  describe('Find Many', () => {
+    it('Should return data with limit', async () => {
+      for (let i = 0; i <= 5; i++) {
+        const modelToInsert = new WeightModel(createWeightData())
+        await repository.insert(modelToInsert)
+      }
+      const result = await repository.findMany(new FindParams({ limit: 5 }))
+      expect(result).toBeInstanceOf(Array)
+      expect(result.length).toEqual(5)
+    })
+  })
+
+  describe('Update', () => {
+    it('Should update data successfully', async () => {
+      const resultUpdate = await repository.update(actualModel.id, { value: 99.99 })
+      expect(resultUpdate.success).toBeTruthy()
+      const findResult = await repository.findOne(actualModel.id)
+      expect(findResult).toBeTruthy()
+      if (findResult) expect(findResult.value).toEqual(99.99)
+    })
+  })
+
+  describe('Delete', () => {
+    it('Should delete data successfully', async () => {
+      const deleteResult = await repository.delete(actualModel.id)
+      expect(deleteResult.success).toBeTruthy()
+      const findResult = await repository.findOne(actualModel.id)
+      expect(findResult).toBeUndefined()
     })
   })
 })
