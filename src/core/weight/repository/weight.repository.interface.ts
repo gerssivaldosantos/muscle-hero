593e1a33bd7abafcfd50ab6787d8550a51c89190
gerssivaldo <canalandows@gmail.com>
2023-07-01 03:42:57 -0300

    chore: delete weight.repository.interface.ts

--- src/core/weight/repository/weight.repository.interface.ts
+++
@@ -1,5 +0,0 @@
-import { RepositoryInterface } from 'src/core/base/repository/repository.interface'
-import { WeightModel } from 'src/core/weight/model/weight.model'
-
-type WeightRepositoryInterface = RepositoryInterface<WeightModel>
-export default WeightRepositoryInterface
