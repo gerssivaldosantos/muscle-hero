8f0fc364487aa167f8d6ef627d7fec97912fcb33
gerssivaldo <canalandows@gmail.com>
2023-07-01 03:22:15 -0300

    feat: create update and delete methods

--- src/core/base/repository/implementations/firebase.repository.ts
+++ src/core/base/repository/implementations/firebase.repository.ts
@@ -1,33 +1,51 @@
 import { EntityModelBase } from 'src/core/base/model/entity.model'
-import { collection, doc, getDocs, query, setDoc, getDoc, Firestore } from 'firebase/firestore'
-import { deleteActionResponse, FindParams, RepositoryInterface } from 'src/core/base/repository/repository.interface'
+import {
+  collection,
+  deleteDoc,
+  doc,
+  Firestore,
+  getDoc,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  setDoc,
+  updateDoc
+} from 'firebase/firestore'
+import { FindParams, repositoryActionResponse, RepositoryInterface } from 'src/core/base/repository/repository.interface'
+import { CollectionReference } from '@firebase/firestore'
 
 export class FirebaseRepository<T extends EntityModelBase> implements RepositoryInterface<T> {
   private readonly collectionName: string
   private readonly database: Firestore
+  private readonly collectionRef: CollectionReference
 
   constructor (collectionName: string, database: Firestore) {
     this.collectionName = collectionName
     this.database = database
+    this.collectionRef = collection(this.database, this.collectionName)
   }
 
-  async insert (item: T): Promise<T | undefined> {
+  async insert (item: T): Promise<repositoryActionResponse> {
     try {
-      const collectionRef = collection(this.database, this.collectionName)
-      await setDoc(doc(collectionRef, item.id), { ...item })
-      return item
-    } catch (error) {
-      console.error('Erro ao inserir item:', error)
-      return undefined
+      await setDoc(doc(this.collectionRef, item.id), { ...item })
+      return { success: true }
+    } catch (error: unknown) {
+      return { success: false, reason: `${error}` }
     }
   }
 
-  delete (id: string): Promise<deleteActionResponse> {
-    return Promise.resolve({ success: false })
+  async delete (id: string): Promise<repositoryActionResponse> {
+    try {
+      await deleteDoc(doc(this.collectionRef, id))
+      return { success: true }
+    } catch (error) {
+      return { success: false, reason: `${error}` }
+    }
   }
 
   async findMany (params: FindParams): Promise<T[]> {
-    const q = query(collection(this.database, this.collectionName)/*, where('capital', '==', true) */)
+    const q = query(this.collectionRef, orderBy(params.orderBy), limit(params.limit)/*, where('capital', '==', true) */)
     const querySnapshot = await getDocs(q)
     const result = querySnapshot.docs.map((item) => item.data()) as T[]
     return Promise.resolve(result)
@@ -36,7 +54,6 @@ export class FirebaseRepository<T extends EntityModelBase> implements Repository
   async findOne (id: string): Promise<T | undefined> {
     const docRef = doc(this.database, this.collectionName, id)
     const docSnap = await getDoc(docRef)
-
     if (docSnap.exists()) {
       return docSnap.data() as T
     } else {
@@ -44,7 +61,12 @@ export class FirebaseRepository<T extends EntityModelBase> implements Repository
     }
   }
 
-  update (id: string, item: T): Promise<boolean> {
-    return Promise.resolve(false)
+  async update (id: string, item: T): Promise<repositoryActionResponse> {
+    try {
+      await updateDoc(doc(this.database, this.collectionName, id), item as object)
+      return { success: true }
+    } catch (error) {
+      return { success: false, reason: `${error}` }
+    }
   }
 }
