0b4dcce3ed5f0830f722da3ec72951c66605f9cc
gerssivaldo <canalandows@gmail.com>
2023-07-01 03:19:08 -0300

    feat: enhance FindParams

--- src/core/base/repository/repository.interface.ts
+++ src/core/base/repository/repository.interface.ts
@@ -1,12 +1,53 @@
-export class FindParams {
+
+type findParamsConstrutorParams = {
   limit?: number
   offset?: number
   query?: Record<string, any>
+  orderBy?: string
+}
+export class FindParams {
+  private _limit: number
+  private _offset: number
+  private _query: Record<string, any>
+  private _orderBy: string
 
-  constructor (params?: FindParams) {
+  get orderBy (): string {
+    return this._orderBy
+  }
+
+  set orderBy (value: string) {
+    this._orderBy = value
+  }
+
+  get query (): Record<string, any> {
+    return this._query
+  }
+
+  set query (value: Record<string, any>) {
+    this._query = value
+  }
+
+  get offset (): number {
+    return this._offset
+  }
+
+  set offset (value: number) {
+    this._offset = value
+  }
+
+  get limit (): number {
+    return this._limit
+  }
+
+  set limit (value: number) {
+    this._limit = value
+  }
+
+  constructor (params?: findParamsConstrutorParams) {
     this.limit = params?.limit || 10
     this.offset = params?.offset || 0
-    this.query = params?.query
+    this.query = params?.query || {}
+    this.orderBy = params?.orderBy || 'id'
   }
 }
 
