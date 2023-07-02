e085da0aa705bd45edaf2a59a939efa139c5378f
gerssivaldo <canalandows@gmail.com>
2023-07-02 20:19:11 -0300

    feat: add vitest coverage

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,14 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  resolve: {
-    alias: [
-      { find: 'src', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
-    ]
+  test: {
+    coverage: {
+      provider: 'v8' // or 'v8'
+    },
+    resolve: {
+      alias: [
+        { find: 'src', replacement: fileURLToPath(new URL('./src', import.meta.url)) }
+      ]
+    }
   }
 })
